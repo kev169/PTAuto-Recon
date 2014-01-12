49f57ac393766519e9a6ccd9887fe5a9eb89e2f7
kevin <kevin@KevDeb.KevDeb>
2014-01-12 02:45:05 -0600

    Added simple start of install script

---
+++ install.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os, sys
+
+os.system("git clone git@github.com:kev169/metagoofil.git ./Tools/metagoofil")
+os.system("git clone git@github.com:kev169/theHarvester.git ./Tools/theHarvester")
+print("Please download latest Linux ZAP scanner from here : http://sourceforge.net/projects/zaproxy/files/")
+print("When done extract it and put it in Tools subfolder and rename folder ZAP")
+
+print("Please install the following")
+print("python-nmap python-msgpack") 
